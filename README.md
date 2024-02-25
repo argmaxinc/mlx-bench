f905125c766b3be5ae8121633beb2dc85754383e
atila <atiorh@icloud.com>
2024-02-25 00:03:01 -0800

    Ready to test

--- README.md
+++ README.md
@@ -5,17 +5,18 @@ Benchmark MLX Performance across forks and commits
 
 ```bash
 pip install -r requirements.txt
+git submodule update
 ```
 
 ## Usage
 
-Example command:
+Example command to test [ml-explore/mlx#735](https://github.com/ml-explore/mlx/pull/735) for Mistral 7b:
 
 ```bash
-python bench_mistral.py
-    --repo-a ml-explore/mlx --commit-a 22364c40b7e488a81c322dfe5663a03daf3190a8
-    --repo-b argmaxinc/mlx --commit-b 5bf706ad86cbe655f8efb1d5e84316e6efb35593
+python bench_mistral.py \
+    --repo-a ml-explore/mlx --commit-a 22364c40b7e488a81c322dfe5663a03daf3190a8 \
+    --repo-b argmaxinc/mlx --commit-b 5bf706ad86cbe655f8efb1d5e84316e6efb35593 \
     --output-dir external --hub-model-name mlx-community/Mistral-7B-Instruct-v0.2-4-bit
 ```
 
-This benchmark tests the two repo+commit pairs for correctness and plot the performance differential in `bench.png`. Useful for benchmarking pull requests such as [ml-explore/mlx#735](https://github.com/ml-explore/mlx/pull/735).
+This benchmark tests the two repo+commit pairs for correctness and plot the performance differential in `bench.png`.
--- bench_mistral.py
+++ bench_mistral.py
@@ -8,12 +8,34 @@ import matplotlib.pyplot as plt
 logger = utils.get_logger(__name__)
 
 SETUP_CMD = "env CMAKE_BUILD_PARALLEL_LEVEL="" pip install -e ."
-MAX_CONTEXT = 10000
+MAX_CONTEXT = 16000
 MEASURE_EVERY_N_TOKENS = 100
-BENCH_CMD = "python llms/mistral/mistral.py"
-" --model-path ../external/model "
-f" --max-tokens {MAX_CONTEXT} "
-f" --tokens-per-eval {MEASURE_EVERY_N_TOKENS}"
+MIN_MISMATCH_AFTER_N_TOKENS = 100
+PROMPT = "Continue this series forever: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10"
+BENCH_CMD = "python llms/mistral/mistral.py" + \
+    " --model-path ../external/model " + \
+    f" --max-tokens {MAX_CONTEXT} " + \
+    f" --tokens-per-eval {MEASURE_EVERY_N_TOKENS}" + \
+    f" --prompt '{PROMPT}'"
+
+# To compute speed-of-light inference speed
+MAC_MEMORY_BW = {
+    "M1": 68.3,
+    "M1 Pro": 200,
+    "M1 Max": 400,
+    "M1 Ultra": 800,
+    "M2": 100,
+    "M2 Pro": 200,
+    "M2 Max": 400,
+    "M2 Ultra": 800,
+    "M3": 100,
+    "M3 Pro": 150,
+    "M3 Max": {"40": 400, "30": 300},
+}
+
+
+def get_speed_of_light_inference_speed(inference_ctx, model):
+    pass
 
 
 def setup_mlx_repos(args):
@@ -52,61 +74,80 @@ def download_model(args):
 def bench(args):
     bench_data = {}
 
-    # # Install repo A
-    # logger.info("Installing repo A")
-    # subprocess.check_call(SETUP_CMD, shell=True, cwd=os.path.join(args.output_dir, "repo_a", args.repo_a.split("/")[1]))
-
-    # Benchmarking --bench-cmd under repo A
-    logger.info("Benchmarking repo A")
-    subprocess.check_call(
-        args.bench_cmd + " --benchmark-json-path benchmark_a.json " + args.bench_cmd_extra_args_a,
-        shell=True, cwd=os.path.join(os.getcwd(), "mlx-examples"))
-
-    # Load benchmark data from repo A
-    with open(os.path.join(os.getcwd(), "mlx-examples", "benchmark_a.json")) as f:
-        bench_data["repo_a"] = json.load(f)
-
-    # # Install repo B
-    # logger.info("Installing repo B")
-    # subprocess.check_call(SETUP_CMD, shell=True, cwd=os.path.join(args.output_dir, "repo_b", args.repo_a.split("/")[1]))
+    # Install repo B
+    logger.info("Installing repo B")
+    subprocess.check_call(SETUP_CMD, shell=True, cwd=os.path.join(args.output_dir, "repo_b", args.repo_a.split("/")[1]))
 
     # Benchmarking --bench-cmd under repo B
     logger.info("Benchmarking repo B")
     subprocess.check_call(
-        args.bench_cmd + " --benchmark-json-path benchmark_b.json --optimized-sdpa",
+        BENCH_CMD + " --benchmark-json-path benchmark_b.json --optimized-sdpa",
         shell=True, cwd=os.path.join(os.getcwd(), "mlx-examples"))
 
     # Load benchmark data from repo B
-    with open(os.path.join(os.getcwd(), "mlx-examples", "benchmark_b.json")) as f:
+    with open(os.path.join(os.getcwd(), "mlx-examples", "benchmark_b.json"), "r") as f:
         bench_data["repo_b"] = json.load(f)
+
+    # Install repo A
+    logger.info("Installing repo A")
+    subprocess.check_call(SETUP_CMD, shell=True, cwd=os.path.join(args.output_dir, "repo_a", args.repo_a.split("/")[1]))
+
+    # Benchmarking --bench-cmd under repo A
+    logger.info("Benchmarking repo A")
+    subprocess.check_call(
+        BENCH_CMD + " --benchmark-json-path benchmark_a.json ",
+        shell=True, cwd=os.path.join(os.getcwd(), "mlx-examples"))
+
+    # Load benchmark data from repo A
+    with open(os.path.join(os.getcwd(), "mlx-examples", "benchmark_a.json"), "r") as f:
+        bench_data["repo_a"] = json.load(f)
 
     return bench_data
 
 
 def check_correctness(bench_data):
     # Check correctness
-    for result_a, result_b in zip(bench_data["repo_a"], bench_data["repo_b"]):
+    mismatch_after_n_tokens = None
+    for idx, (result_a, result_b) in enumerate(zip(bench_data["repo_a"], bench_data["repo_b"])):
         if not result_a[-1] == result_b[-1]:
             logger.error(f"Mismatch in results: {result_a[-1]} vs {result_b[-1]}")
+            mismatch_after_n_tokens = idx * MEASURE_EVERY_N_TOKENS
+            logger.info(f"First mismatch after n tokens: {mismatch_after_n_tokens}")
+            if mismatch_after_n_tokens < MIN_MISMATCH_AFTER_N_TOKENS:
+                raise ValueError(
+                    f"First mismatch after {mismatch_after_n_tokens} tokens "
+                    f"(Less than {MIN_MISMATCH_AFTER_N_TOKENS})")
+            break
         else:
             logger.info(f"Results match: {result_a[-1]} vs {result_b[-1]}")
+    return mismatch_after_n_tokens
 
 
 def plot_performance(bench_data, inference_ctx, args):
     # Plot performance
-    fig, ax = plt.subplots()
-    ax.plot([x[0] for x in bench_data["repo_a"]], [x[1] for x in bench_data["repo_a"]], label=f"{args.repo_a}@{args.commit_a[:7]}")
-    ax.plot([x[0] for x in bench_data["repo_b"]], [x[1] for x in bench_data["repo_b"]], label=f"{args.repo_b}@{args.commit_b[:7]}")
-    ax.set_xlabel("Context Length (tokens)")
-    ax.set_ylabel("Inference Speed (tokens/second)")
-    ax.set_title(
-        f"{inference_ctx['device_spec']['product_name']} "
-        f"({inference_ctx['device_spec']['gpu_core_count']} GPU cores, "
+    f, ax = plt.subplots(1, 2, figsize=(13, 6))
+    ax[0].plot([x[0] for x in bench_data["repo_a"]], [x[1] for x in bench_data["repo_a"]], label=f"{args.repo_a}@{args.commit_a[:7]}")
+    ax[0].plot([x[0] for x in bench_data["repo_b"]], [x[1] for x in bench_data["repo_b"]], label=f"{args.repo_b}@{args.commit_b[:7]}")
+    ax[0].set_xlabel("Context Length (tokens)")
+    ax[0].set_ylabel("Inference Speed (tokens/second)")
+    ax[0].set_title(
+        f"Model={args.hub_model_name} \n"
+        f"Device=({inference_ctx['device_spec']['product_name']},"
+        f" {inference_ctx['device_spec']['gpu_core_count']} GPU cores, "
         f"macOS={inference_ctx['os_spec']['os_build_number']})"
     )
-    ax.legend()
+    ax[0].legend()
 
-    plt.savefig("bench.png")
+    ax[1].set_ylabel("Speedup")
+    ax[1].set_xlabel("Context Length (tokens)")
+    ax[1].plot(
+        [x[0] for x in bench_data["repo_a"]],
+        [y[1]/x[1] for x, y in zip(bench_data["repo_a"], bench_data["repo_b"])],
+    )
+
+    # TODO(atiorh): Plot speed-of-light inference speed based on memory bandwidth
+
+    f.savefig("bench.png")
 
 
 if __name__ == "__main__":
@@ -121,7 +162,11 @@ if __name__ == "__main__":
     parser.add_argument(
         "--hub-model-name",
         type=str,
-        choices=("mlx-community/Mistral-7B-Instruct-v0.2-4-bit",)
+        choices=(
+            "mlx-community/Mistral-7B-Instruct-v0.2",
+            "mlx-community/Mistral-7B-Instruct-v0.2-4-bit",
+            "mlx-community/Mistral-7B-Instruct-v0.2-8-bit",
+        )
     )
 
     args = parser.parse_args()
@@ -144,7 +189,5 @@ if __name__ == "__main__":
     download_model(args)
 
     bench_data = bench(args)
-    check_correctness(bench_data)
+    mismatch_after_n_tokens = check_correctness(bench_data)
     plot_performance(bench_data, inference_ctx, args)
-
-    from IPython import embed; embed()
